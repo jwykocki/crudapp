f96a29a5b20e369cc4dc54aada4c47c4c52074b0
Kuba <jakub.wykocki@wp.pl>
2023-10-09 15:24:29 +0200

    EmployeeService refactoring

--- src/main/java/com/example/springboot/crudapp/dao/EmployeeRepository.java
+++ src/main/java/com/example/springboot/crudapp/dao/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.example.springboot.crudapp.dao;
 
 import com.example.springboot.crudapp.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
